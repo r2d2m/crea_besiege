170179ac37758b60a2bb27e86392406245d5040c
Vincent <vincent.breysse99@gmail.com>
2019-12-13 10:53:33 +0100

    Implement block chaining

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	[SerializeField] GameObject goBlock;
+
+	string blockMask = "Block";
+	Camera mainCam;
+
+    void Start()
+    {
+		this.mainCam = Camera.main;   
+    }
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			var ray = this.mainCam.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask(this.blockMask)))
+			{
+				var box = hit.transform.GetComponent<BoxCollider>();
+				Debug.Assert(box != null);
+
+				var offset = hit.normal;
+				offset.x *= box.size.x / 2f;
+				offset.y *= box.size.y / 2f;
+				offset.z *= box.size.z / 2f;
+
+				var pos = box.transform.position + offset;
+
+				Instantiate(this.goBlock, pos, Quaternion.identity);
+			}
+		}
+    }
+}
