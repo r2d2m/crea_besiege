9dd28da83e801cc539265009398bf463b0dce898
Vincent <vincent.breysse99@gmail.com>
2019-12-20 18:20:47 +0100

    Add Helper class

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+	public static readonly int DefaultLayerMask = (~0) ^ LayerMask.GetMask("Ignored");
+	public static readonly int BlockLayerMask = LayerMask.GetMask("Block");
+
+	public static Vector3 Multiplied(this Vector3 self, Vector3 vec)
+	{
+		return new Vector3(self.x * vec.x, self.y * vec.y, self.z * vec.z);
+	}
+}
