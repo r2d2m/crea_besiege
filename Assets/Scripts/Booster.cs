79055ecd2237dad0f0568be6fa255d52c0dc344f
Vincent <vincent.breysse99@gmail.com>
2019-12-20 20:39:07 +0100

    Implement Booster

---
+++ Assets/Scripts/Booster.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Booster : MonoBehaviour, IAttachable
+{
+	[SerializeField] BoxCollider box;
+
+	public float projectionForce = 2f;
+
+	Rigidbody body;
+
+	private void Awake()
+	{
+		this.body = GetComponent<Rigidbody>();
+	}
+
+	void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+		{
+			Use();
+		}
+    }
+
+	public void Use()
+	{
+		this.body.AddForce(this.Projection);
+	}
+
+	public void Attach(Block block, Vector3 direction)
+	{
+		this.transform.rotation = Quaternion.FromToRotation(-this.ProjectionDirection, direction);
+		Physics.SyncTransforms();
+
+		Vector3 translation = direction.Multiplied(block.Bounds.extents + this.Bounds.extents);
+
+		this.transform.position = block.Bounds.center + translation;
+
+		var joint = this.gameObject.AddComponent<FixedJoint>();
+		joint.connectedBody = block.RigidBody;
+	}
+
+	public Vector3 ProjectionDirection
+	{
+		get => -this.transform.up;
+	}
+
+	public Vector3 Projection
+	{
+		get => this.ProjectionDirection * this.projectionForce;
+	}
+
+	public GameObject GameObject
+	{
+		get => this.gameObject;
+	}
+
+	public Bounds Bounds
+	{
+		get => this.box.bounds;
+	}
+}
