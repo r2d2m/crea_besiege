1a85d92415f11a270849f1cecd43e51668b3aa9e
Vincent <vincent.breysse99@gmail.com>
2019-12-21 17:31:20 +0100

    Add a VictoryZone

---
+++ Assets/Scripts/VictoryZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryZone : MonoBehaviour
+{
+	public string nextLevel;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		var block = other.gameObject.GetComponent<Block>();
+
+		if (block != null)
+		{
+			Helper.LoadSingleActiveScene(this.nextLevel);
+		}
+	}
+}
