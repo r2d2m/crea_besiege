44bcd69ee892d7bf5c2d04d5fe941784b032ccce
Vincent <vincent.breysse99@gmail.com>
2019-12-21 17:30:46 +0100

    Add DebugInputs script to help scene transitions testing

---
+++ Assets/Scripts/DebugInputs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugInputs : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+	void Start()
+    {
+		
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+		{
+			Helper.LoadSingleActiveScene("World1");
+		}
+    }
+
+#endif
+}
