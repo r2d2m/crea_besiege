50846e256509926dba0af40918bb096eea303f5e
Vincent <vincent.breysse99@gmail.com>
2019-12-21 17:29:09 +0100

    Add helper function to replace active scene

--- Assets/Scripts/Helper.cs
+++ Assets/Scripts/Helper.cs
@@ -1,15 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class Helper
 {
 	public const string PersistentSceneName = "Persistent";
+
 	public static readonly int DefaultLayerMask = (~0) ^ LayerMask.GetMask("Ignored");
+
 	public static readonly int BlockLayerMask = LayerMask.GetMask("Block");
 
 	public static Vector3 Multiplied(this Vector3 self, Vector3 vec)
 	{
 		return new Vector3(self.x * vec.x, self.y * vec.y, self.z * vec.z);
+	}
+
+	public static void LoadAsActiveScene(string name)
+	{
+		AsyncOperation loadOp = SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+
+		loadOp.completed += (dummy) =>
+		{
+			SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
+		};
+	}
+
+	public static void LoadSingleActiveScene(string name)
+	{
+		Scene activeScene = SceneManager.GetActiveScene();
+
+		if (activeScene.name != PersistentSceneName)
+		{
+			AsyncOperation unloadOp = SceneManager.UnloadSceneAsync(activeScene);
+
+			unloadOp.completed += (dummy) =>
+			{
+				LoadAsActiveScene(name);
+			};
+		}
+		else
+		{
+			LoadAsActiveScene(name);
+		}
 	}
 }
