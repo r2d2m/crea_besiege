30e37126176a78b775f61609a8e583f95c208f3e
Vincent <vincent.breysse99@gmail.com>
2019-12-21 18:55:47 +0100

    Add OrbitalCamera prefab ; Improve EditionUI pefab and script

--- Assets/Scripts/EditionUI.cs
+++ Assets/Scripts/EditionUI.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditionUI : MonoBehaviour
 {
+	[SerializeField] private Button solidBlockButton;
+	[SerializeField] private Button wheelButton;
+	[SerializeField] private Button boosterButton;
+
+	[SerializeField] private GameObject solidBlockPrefab;
+	[SerializeField] private GameObject wheelPrefab;
+	[SerializeField] private GameObject boosterPrefab;
+
     void Start()
     {
-        
-    }
+		this.solidBlockButton.onClick.AddListener(() =>
+		{
+			Refs.controller.SetHand(this.solidBlockPrefab);
+		});
+
+		this.wheelButton.onClick.AddListener(() =>
+		{
+			Refs.controller.SetHand(this.wheelPrefab);
+		});
+
+		this.boosterButton.onClick.AddListener(() =>
+		{
+			Refs.controller.SetHand(this.boosterPrefab);
+		});
+	}
 
     void Update()
     {
