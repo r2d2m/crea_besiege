8a8d15a1421d5949337a1b5639fc74a69439a677
Vincent <vincent.breysse99@gmail.com>
2019-12-22 21:35:29 +0100

    Add VehicleSpawner

---
+++ Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSpawner : MonoBehaviour
+{
+	public static string Pick = null;
+
+    void Start()
+    {
+        if (Pick != null)
+		{
+			Vehicle.CreateFromJson(Pick);
+		}
+    }
+
+    void Update()
+    {
+        
+    }
+}
