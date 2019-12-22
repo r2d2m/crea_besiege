347718c4d0860b7cc2ab4ca17b0f7a02cf413a2e
Vincent <vincent.breysse99@gmail.com>
2019-12-22 16:41:06 +0100

    Add Menu and empty UI scenes

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+	[SerializeField] Button vehicleSelectionButton;
+	[SerializeField] string vehicleSelectionScene;
+
+	[SerializeField] Button levelScelectionButton;
+	[SerializeField] string levelSelectionScene;
+
+    void Start()
+    {
+		this.vehicleSelectionButton.onClick.AddListener(() =>
+		{
+			Helper.LoadSingleActiveScene(this.vehicleSelectionScene);
+		});
+
+		this.levelScelectionButton.onClick.AddListener(() =>
+		{
+			Helper.LoadSingleActiveScene(this.levelSelectionScene);
+		});
+	}
+
+    void Update()
+    {
+        
+    }
+}
