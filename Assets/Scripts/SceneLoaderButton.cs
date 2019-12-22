ef71e06f24d1aa1085a23ad915d2c2b12588a87b
Vincent <vincent.breysse99@gmail.com>
2019-12-22 21:32:48 +0100

    Add SceneLoaderButton class

---
+++ Assets/Scripts/SceneLoaderButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PushButton))]
+public class SceneLoaderButton : MonoBehaviour
+{
+	[SerializeField] private string sceneName;
+
+	private PushButton button;
+
+	private void Awake()
+	{
+		this.button = GetComponent<PushButton>();
+	}
+
+	private void Start()
+    {
+		this.button.AddListener(() =>
+		{
+			Helper.LoadSingleActiveScene(this.sceneName);
+		});
+    }
+}
